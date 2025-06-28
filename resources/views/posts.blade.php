97253c1f5d2cc1717fa98f7087f90e57d8f6a14a
muhammadfadhil0 <izzanmaula0@gmail.com>
2025-06-28 22:08:08 +0700

    update eksperimen single post

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<x-layout :title="$title">
+
+
+    @foreach ($posts as $post)
+        <div class="mp-6">
+            <article class="p-6 border-b-2 border-grey-10 transition-shadow duration-300">
+                <a href="post/{{ $post['id'] }}">
+                    {{-- judul artikel --}}
+                    <h2 class="text-2xl font-bold text-gray-800 mb-3 hover:text-blue-600 transition-colors">
+                        {{ $post['title'] }}</h2>
+
+                </a>
+                {{-- metadata --}}
+                <div class="text-sm text-gray-500 mb-4">
+                    <p> {{ $post['author'] }} | 8 Agustus 2025</p>
+                </div>
+                {{-- isi artikel --}}
+                <p class=" mb-4">
+                    {{ Str::limit($post['body']), 10 }}
+                </p>
+                <a href="/post/{{ $post['id'] }}" class="font-medium text-blue-600 hover:underline">read
+                    more >></a>
+            </article>
+        </div>
+    @endforeach
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
 
 //root
 
@@ -19,20 +20,46 @@ Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact Page']);
 });
 
-Route::get('/post', function () {
+Route::get('/posts', function () {
 
     $posts = [
         [
+            'id' => 1,
             'title' => 'Apa bahaya merokok?',
             'author' => 'Muhammad Fadhil',
             'body' => ' Merokok adalah kebiasaan yang mengakar di masyarakat global, sering kali dimulai sebagai bentuk rekreasi atau tekanan sosial. Namun, di balik setiap kepulan asap tersimpan ancaman serius yang secara perlahan namun pasti merusak kesehatan penggunanya, bahkan orang-orang di sekitarnya. Mengenali bahaya ini adalah langkah pertama untuk melindungi diri dan orang-orang terkasih dari dampak buruknya.'
         ],
         [
+            'id' => 2,
             'title' => 'belajar koding sejak dini',
             'author' => 'azzura q',
             'body' => ' Di era digital yang terus berkembang pesat, kemampuan koding atau pemrograman bukan lagi sekadar keahlian niche. Ia telah menjadi literasi fundamental, layaknya membaca dan menulis, yang membuka gerbang ke berbagai peluang di masa depan. Memperkenalkan dunia koding kepada anak sejak dini bukan hanya tentang menciptakan programer andal, melainkan juga membekali mereka dengan keterampilan berpikir kritis, logis,'
         ]
     ];
 
-    return view('post', ['title' => 'Blog Page', 'posts' => $posts]);
+    return view('posts', ['title' => 'Blog Page', 'posts' => $posts]);
+});
+
+Route::get('/post/{id}', function ($id) {
+
+    $posts = [
+        [
+            'id' => 1,
+            'title' => 'Apa bahaya merokok?',
+            'author' => 'Muhammad Fadhil',
+            'body' => ' Merokok adalah kebiasaan yang mengakar di masyarakat global, sering kali dimulai sebagai bentuk rekreasi atau tekanan sosial. Namun, di balik setiap kepulan asap tersimpan ancaman serius yang secara perlahan namun pasti merusak kesehatan penggunanya, bahkan orang-orang di sekitarnya. Mengenali bahaya ini adalah langkah pertama untuk melindungi diri dan orang-orang terkasih dari dampak buruknya.'
+        ],
+        [
+            'id' => 2,
+            'title' => 'belajar koding sejak dini',
+            'author' => 'azzura q',
+            'body' => ' Di era digital yang terus berkembang pesat, kemampuan koding atau pemrograman bukan lagi sekadar keahlian niche. Ia telah menjadi literasi fundamental, layaknya membaca dan menulis, yang membuka gerbang ke berbagai peluang di masa depan. Memperkenalkan dunia koding kepada anak sejak dini bukan hanya tentang menciptakan programer andal, melainkan juga membekali mereka dengan keterampilan berpikir kritis, logis,'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($id) {
+        return $post['id'] == $id;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post, 'id' => $id]);
 });
